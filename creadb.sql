649690fabde2236d49bfe9c08595a8d0191d46ec
Martin Rados <martin@martin-virtual-machine.(none)>
2013-04-19 16:47:40 -0300

    creadb

--- creadb.sql
+++ creadb.sql
@@ -5,6 +5,8 @@ drop function lista_vuelo(integer, integer, integer);
 drop function lista_vuelos(vuelo in integer,orden_desde in integer, orden_hasta integer);
 drop function una_conexion(origen aeropuerto.aer_codigo%type,destino aeropuerto.aer_codigo%type);
 drop function sin_conexiones(origen aeropuerto.aer_codigo%type,destino aeropuerto.aer_codigo%type);
+drop trigger check_escala on pasa_por;
+drop function check_escala();
 
 drop table conexion;
 drop table pasa_por;
@@ -22,7 +24,7 @@ alter table aeropuerto add primary key (aer_codigo);
 create table vuelo (vue_numero int,hora_salida time);
 alter table vuelo add primary key (vue_numero);
 
-create table pasa_por(vue_numero int,orden int,aer_codigo char(3),tiempo_de_vuelo float,tiempo_de_escala float);
+create table pasa_por(vue_numero int,orden int,aer_codigo char(3),tiempo_de_vuelo interval hour to minute ,tiempo_de_escala interval hour to minute );
 alter table pasa_por add primary key (vue_numero,orden);
 alter table pasa_por add foreign key (vue_numero) references vuelo;
 alter table pasa_por add foreign key (aer_codigo) references aeropuerto;
@@ -31,39 +33,6 @@ create table conexion (vue_numero_llega int,orden_llega int,vue_numero_sale int,
 alter table conexion add primary key (vue_numero_llega,orden_llega,vue_numero_sale,orden_sale);
 alter table conexion add foreign key (vue_numero_llega,orden_llega) references pasa_por(vue_numero,orden);
 alter table conexion add foreign key (vue_numero_sale,orden_sale) references pasa_por(vue_numero,orden);
-
-
---
--- Datos
---
-
-insert into aeropuerto values ('EZE','Aeropuerto Internacional de Ezeiza');
-insert into aeropuerto values ('AEP','Aeroparque CABA');
-insert into aeropuerto values ('MDQ','Aeropuerto de Mar del Plata');
-insert into aeropuerto values ('MZA','Aeropuerto de Mendoza');
-insert into aeropuerto values ('CBA','Aeropuerto de Cordoba');
-insert into aeropuerto values ('SFE','Aeropuerto de Sta Fe');
-insert into aeropuerto values ('ROS','Aeropuerto de Rosario');
-
-insert into vuelo values (1109,'10:00');
-insert into vuelo values (1110,'10:00');
-insert into vuelo values (1111,'10:00');
-
-insert into pasa_por values (1109,1,'AEP',null,1);
-insert into pasa_por values (1109,2,'ROS',1,1);
-insert into pasa_por values (1109,3,'MDQ',1,1);
-insert into pasa_por values (1109,4,'CBA',1,null);
-
-insert into pasa_por values (1110,1,'AEP',null,1);
-insert into pasa_por values (1110,2,'MDQ',1,1);
-insert into pasa_por values (1110,3,'CBA',2,null);
-
-insert into pasa_por values (1111,1,'EZE',null,1);
-insert into pasa_por values (1111,2,'MDQ',1,1);
-insert into pasa_por values (1111,3,'MZA',2,null);
-
-insert into conexion values (1109,3,1111,2);
-insert into conexion values (1110,2,1111,2);
 
 
 --
@@ -148,3 +117,56 @@ begin
   return;
 end;
 $$ language plpgsql;
+
+
+--
+-- Triggers
+--
+create function check_escala() returns trigger as 
+$$
+begin
+  if new.tiempo_de_vuelo < '00:00' then
+    raise exception 'Tiempo de vuelo no puede ser negativo.';
+  end if;
+  if new.tiempo_de_escala < '00:00' then
+    raise exception 'Tiempo de escala no puede ser negativo.';
+  end if;
+  return new;
+ end;
+$$ language plpgsql;
+
+create trigger check_escala before insert or update on pasa_por
+    for each row execute procedure check_escala();
+
+
+--
+-- Datos
+--
+
+insert into aeropuerto values ('EZE','Aeropuerto Internacional de Ezeiza');
+insert into aeropuerto values ('AEP','Aeroparque CABA');
+insert into aeropuerto values ('MDQ','Aeropuerto de Mar del Plata');
+insert into aeropuerto values ('MZA','Aeropuerto de Mendoza');
+insert into aeropuerto values ('CBA','Aeropuerto de Cordoba');
+insert into aeropuerto values ('SFE','Aeropuerto de Sta Fe');
+insert into aeropuerto values ('ROS','Aeropuerto de Rosario');
+
+insert into vuelo values (1109,'10:00');
+insert into vuelo values (1110,'10:00');
+insert into vuelo values (1111,'10:00');
+
+insert into pasa_por values (1109,1,'AEP',null,'01:00');
+insert into pasa_por values (1109,2,'ROS','01:00','01:00');
+insert into pasa_por values (1109,3,'MDQ','01:00','01:00');
+insert into pasa_por values (1109,4,'CBA','01:00',null);
+
+insert into pasa_por values (1110,1,'AEP',null,'01:00');
+insert into pasa_por values (1110,2,'MDQ','01:00','01:00');
+insert into pasa_por values (1110,3,'CBA','02:00',null);
+
+insert into pasa_por values (1111,1,'EZE',null,'01:00');
+insert into pasa_por values (1111,2,'MDQ','01:00','01:00');
+insert into pasa_por values (1111,3,'MZA','02:00',null);
+
+insert into conexion values (1109,3,1111,2);
+insert into conexion values (1110,2,1111,2);
