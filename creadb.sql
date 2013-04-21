a4aa699b03d6da1713b68682624911ee4a6ecf6d
Martin Rados <radosm@gmail.com>
2013-04-21 11:06:43 -0300

    agregado creadb.sql (incompleto)

--- creadb.sql
+++ creadb.sql
@@ -1,172 +1,101 @@
---
--- Drops
---
-drop function lista_vuelo(integer, integer, integer);
-drop function lista_vuelos(vuelo in integer,orden_desde in integer, orden_hasta integer);
-drop function una_conexion(origen aeropuerto.aer_codigo%type,destino aeropuerto.aer_codigo%type);
-drop function sin_conexiones(origen aeropuerto.aer_codigo%type,destino aeropuerto.aer_codigo%type);
-drop trigger check_escala on pasa_por;
-drop function check_escala();
-
-drop table conexion;
-drop table pasa_por;
-drop table vuelo;
+drop table avion;
+drop table modelo_avion;
 drop table aeropuerto;
+drop table pais;
+drop table persona_reserva;
+drop table persona_cuenta;
+drop table reserva;
+drop table tarjeta;
+drop table cuenta;
 
+create table cuenta (
+  userid varchar(8),
+  nombre varchar,
+  apellido varchar,
+  clave varchar,
+  email varchar,
+  profesion varchar,
+  telefono varchar,
+  direccion text,
+  direccion_entrega text
+);
 
---
--- Tablas y constraints
---
+alter table cuenta add primary key (userid);
 
-create table aeropuerto (aer_codigo char(3), aer_nombre varchar);
-alter table aeropuerto add primary key (aer_codigo);
+create table tarjeta (
+  numero numeric(16),
+  marca varchar, -- visa, diners, etc
+  banco_emisor varchar, -- bbva
+  vencimiento interval year to month,
+  direccion_facturacion text,
+  userid varchar(8)
+);
 
-create table vuelo (vue_numero int,hora_salida time);
-alter table vuelo add primary key (vue_numero);
+alter table tarjeta add primary key (numero);
+alter table tarjeta add foreign key (userid) references cuenta;
 
-create table pasa_por(vue_numero int,orden int,aer_codigo char(3),tiempo_de_vuelo interval hour to minute ,tiempo_de_escala interval hour to minute );
-alter table pasa_por add primary key (vue_numero,orden);
-alter table pasa_por add foreign key (vue_numero) references vuelo;
-alter table pasa_por add foreign key (aer_codigo) references aeropuerto;
+create table reserva (
+  codigo_reserva char(8),
+  forma_de_pago varchar,
+  userid varchar(8)
+);
 
-create table conexion (vue_numero_llega int,orden_llega int,vue_numero_sale int,orden_sale int);
-alter table conexion add primary key (vue_numero_llega,orden_llega,vue_numero_sale,orden_sale);
-alter table conexion add foreign key (vue_numero_llega,orden_llega) references pasa_por(vue_numero,orden);
-alter table conexion add foreign key (vue_numero_sale,orden_sale) references pasa_por(vue_numero,orden);
+alter table reserva add primary key (codigo_reserva);
+alter table reserva add foreign key (userid) references cuenta;
 
+create table persona_cuenta (
+  tipo_doc varchar(20),
+  nro_doc numeric(20),
+  nombre varchar,
+  apellido varchar,
+  fecha_nac date,
+  nacionalidad varchar,
+  userid varchar(8)
+);
 
---
--- Funciones plpgsql
---
+alter table persona_cuenta add primary key (tipo_doc,nro_doc);
+alter table persona_cuenta add foreign key (userid) references cuenta;
 
---
--- Muestra un vuelo entre dos escalas
---
-create function lista_vuelos(vuelo in integer,orden_desde in integer, orden_hasta integer)
-returns setof pasa_por as
-$$
-begin
-  return query select * from pasa_por where vue_numero=$1 and orden between $2 and $3;
-end;
-$$ language plpgsql;
+create table persona_reserva (
+  tipo_doc varchar(20),
+  nro_doc numeric(20),
+  nombre varchar,
+  apellido varchar,
+  fecha_nac date,
+  nacionalidad varchar,
+  codigo char(8)
+);
 
+alter table persona_reserva add primary key (tipo_doc,nro_doc);
+alter table persona_reserva add foreign key (codigo_reserva) references reserva;
 
---
--- Vuelos sin conexiones
---
-create function sin_conexiones(origen aeropuerto.aer_codigo%type,destino aeropuerto.aer_codigo%type)
-returns setof pasa_por as
-$$
-declare
-  v cursor (origen char(3),destino char(3)) for 
-    select vue_numero from pasa_por pp 
-    where aer_codigo=origen -- Sale de donde quiero
-      and exists (select * from pasa_por           -- Llega hasta donde quiero
-                   where vue_numero=pp.vue_numero 
-                     and aer_codigo=destino
-                     and orden > pp.orden);
-  orden_ori int;
-  orden_dst int;
-begin
-  for vue in v(origen,destino) loop
-    select orden into orden_ori from pasa_por 
-    where vue_numero=vue.vue_numero and aer_codigo=origen;
-  
-    select orden into orden_dst from pasa_por 
-    where vue_numero=vue.vue_numero and aer_codigo=destino;
-  
-    return query select * from lista_vuelos(vue.vue_numero,orden_ori,orden_dst);
-  end loop;
-  return;
-end;
-$$ language plpgsql;
+create table pais (
+  codigo_pais char(2),
+  nombre varchar
+);
 
+alter table pais add primary key (codigo_pais);
 
---
--- Vuelos con una conexion
---
-create function una_conexion(origen aeropuerto.aer_codigo%type,destino aeropuerto.aer_codigo%type)
-returns setof pasa_por as
-$$
-declare
-  cx cursor (origen char(3),destino char(3)) for 
-    select * from conexion cx 
-     where vue_numero_llega in (select vue_numero from pasa_por pp where aer_codigo=origen)
-       and exists (select * from pasa_por 
-                    where vue_numero=cx.vue_numero_sale 
-                      and aer_codigo=destino and orden > cx.orden_sale);
-  desde_llega int;
-  hasta_sale int;
-begin
-  for conex in cx(origen,destino) loop
-    select orden into desde_llega from pasa_por 
-    where vue_numero=conex.vue_numero_llega and aer_codigo=origen;
+create table aeropuerto (
+  codigo_aerop char(3),
+  nombre varchar,
+  codigo_pais char(2)
+);
 
-    select orden into hasta_sale from pasa_por 
-    where vue_numero=conex.vue_numero_sale and aer_codigo=destino;
- 
-    -- No muestra conexiones ridiculas
-    if desde_llega < conex.orden_llega and conex.orden_sale < hasta_sale
-    then
-      return query 
-        select * from lista_vuelos(conex.vue_numero_llega,desde_llega,conex.orden_llega);
-      return query 
-        select * from lista_vuelos(conex.vue_numero_sale,conex.orden_sale,hasta_sale);
-    end if;
-  end loop;
-  return;
-end;
-$$ language plpgsql;
+alter table aeropuerto add primary key (codigo_aerop);
+alter table aeropuerto add foreign key (codigo_pais) references pais;
 
+create table modelo_avion (
+  codigo_modelo varchar,
+  descripcion text
+);
 
---
--- Triggers
---
-create function check_escala() returns trigger as 
-$$
-begin
-  if new.tiempo_de_vuelo < '00:00' then
-    raise exception 'Tiempo de vuelo no puede ser negativo.';
-  end if;
-  if new.tiempo_de_escala < '00:00' then
-    raise exception 'Tiempo de escala no puede ser negativo.';
-  end if;
-  return new;
- end;
-$$ language plpgsql;
+alter table modelo_avion add primary key (codigo_modelo);
 
-create trigger check_escala before insert or update on pasa_por
-    for each row execute procedure check_escala();
+create table avion (
+  codigo_avion varchar,
+  anio_fabric interval year,
+  millas integer
+);
 
-
---
--- Datos
---
-
-insert into aeropuerto values ('EZE','Aeropuerto Internacional de Ezeiza');
-insert into aeropuerto values ('AEP','Aeroparque CABA');
-insert into aeropuerto values ('MDQ','Aeropuerto de Mar del Plata');
-insert into aeropuerto values ('MZA','Aeropuerto de Mendoza');
-insert into aeropuerto values ('CBA','Aeropuerto de Cordoba');
-insert into aeropuerto values ('SFE','Aeropuerto de Sta Fe');
-insert into aeropuerto values ('ROS','Aeropuerto de Rosario');
-
-insert into vuelo values (1109,'10:00');
-insert into vuelo values (1110,'10:00');
-insert into vuelo values (1111,'10:00');
-
-insert into pasa_por values (1109,1,'AEP',null,'01:00');
-insert into pasa_por values (1109,2,'ROS','01:00','01:00');
-insert into pasa_por values (1109,3,'MDQ','01:00','01:00');
-insert into pasa_por values (1109,4,'CBA','01:00',null);
-
-insert into pasa_por values (1110,1,'AEP',null,'01:00');
-insert into pasa_por values (1110,2,'MDQ','01:00','01:00');
-insert into pasa_por values (1110,3,'CBA','02:00',null);
-
-insert into pasa_por values (1111,1,'EZE',null,'01:00');
-insert into pasa_por values (1111,2,'MDQ','01:00','01:00');
-insert into pasa_por values (1111,3,'MZA','02:00',null);
-
-insert into conexion values (1109,3,1111,2);
-insert into conexion values (1110,2,1111,2);
+alter table avion add primary key (codigo_avion);
