ca71139fae5654e9a4f233de6095fc53b670a02b
MauricioAlfonso <mauricio.alfonso.88@gmail.com>
2013-04-24 16:19:16 -0300

    pasajeros.sql cambiado y achicado informe

--- pasajeros.sql
+++ pasajeros.sql
@@ -1,49 +1,28 @@
---asumo que las reservas no concretadas se borran!!
+drop view if exists vw_paso_por;
 
---buscar persona...
-select nombre, apellido, nro_doc, tipo_doc from persona per where not exists (
-	--tal que no exista ningun pais...
-	select * from pais pa where not exists (
-		--tal que la persona no estuvo en un viaje con origen en ese país en los últimos 5 años
-		select * from persona_reserva pr 
-		join reserva res on pr.codigo_reserva = res.codigo_reserva 
-		join reserva_viaje rv on rv.codigo_reserva = res.codigo_reserva
-		join viaje via on rv.fecha_viaje = via.fecha_viaje and rv.numero_vuelo = via.numero_vuelo
-		join vuelo vue on vue.numero_vuelo = via.numero_vuelo
-		join aeropuerto ori on vue.aeropuerto_ori = ori.codigo_aerop
-		where 
-			pr.nro_doc = per.nro_doc and
-			pr.tipo_doc = per.tipo_doc and
-			ori.codigo_pais = pa.codigo_pais and
-			via.fecha_viaje > (
-				select now() - interval '5 years'
-			)
-	) and not exists (
-		--ni tampoco en un viaje con destino a ese país
-		select * from persona_reserva pr 
-		join reserva res on pr.codigo_reserva = res.codigo_reserva 
-		join reserva_viaje rv on rv.codigo_reserva = res.codigo_reserva
-		join viaje via on rv.fecha_viaje = via.fecha_viaje and rv.numero_vuelo = via.numero_vuelo
-		join vuelo vue on vue.numero_vuelo = via.numero_vuelo
-		join aeropuerto dest on vue.aeropuerto_dst = dest.codigo_aerop
-		where 
-			pr.nro_doc = per.nro_doc and
-			pr.tipo_doc = per.tipo_doc and
-			dest.codigo_pais = pa.codigo_pais and
-			via.fecha_viaje > (
-				select now() - interval '5 years'
-			)
-	) and (
-		exists (
-			--y que halla algun vuelo que tenga ese país como origen
-			select * from vuelo vue 
-			join aeropuerto ori on vue.aeropuerto_ori = ori.codigo_aerop
-			where pa.codigo_pais = ori.codigo_pais
-		) or exists (
-			--o como destino
-			select * from vuelo vue 
-			join aeropuerto dest on vue.aeropuerto_dst = dest.codigo_aerop
-			where pa.codigo_pais = dest.codigo_pais
-		)
-	)
-)
+--
+-- reservas confirmadas, origen y destino
+--
+create or replace view vw_paso_por as
+  select r.codigo_reserva,rv.fecha_viaje+v.hora_despegue salio,v.aeropuerto_ori,rv.fecha_viaje+v.hora_despegue+v.duracion llego,v.aeropuerto_dst
+  from reserva r, reserva_viaje rv, vuelo v
+  where rv.codigo_reserva=r.codigo_reserva
+  and v.numero_vuelo=rv.numero_vuelo
+  and r.estado='confirmado';
+
+--
+-- Personas que viajaron a todos los destinos en los últimos 5 años
+--
+select * from persona where (tipo_doc,nro_doc) in (
+  select p.tipo_doc,p.nro_doc
+  from vw_paso_por pp, persona_reserva pr, persona p, aeropuerto a
+  where pp.llego between (current_date - interval '5 year') and current_date
+    and pr.codigo_reserva=pp.codigo_reserva
+    and p.tipo_doc=pr.tipo_doc
+    and p.nro_doc=pr.nro_doc
+    and a.codigo_aerop in (pp.aeropuerto_ori,pp.aeropuerto_dst)
+  group by p.tipo_doc,p.nro_doc
+  having count(distinct a.codigo_pais)=(select count(distinct a.codigo_pais)
+                                        from vuelo v,aeropuerto a
+                                        where a.codigo_aerop in (v.aeropuerto_ori,v.aeropuerto_dst))
+);
