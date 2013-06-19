c73e014bc9b4cc6f7f4786bfa249163fded6ef30
Martin Rados <radosm@gmail.com>
2013-06-19 17:56:53 -0300

    final?

--- TP2/UBADB/src/main/java/ubadb/external/bufferManagement/MainEvaluator.java
+++ TP2/UBADB/src/main/java/ubadb/external/bufferManagement/MainEvaluator.java
@@ -1,14 +1,19 @@
 package ubadb.external.bufferManagement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ubadb.core.components.bufferManager.BufferManager;
 import ubadb.core.components.bufferManager.BufferManagerException;
 import ubadb.core.components.bufferManager.BufferManagerImpl;
 import ubadb.core.components.bufferManager.bufferPool.BufferPool;
+import ubadb.core.components.bufferManager.bufferPool.pools.multiple.MultipleBufferPool;
 import ubadb.core.components.bufferManager.bufferPool.pools.single.SingleBufferPool;
 import ubadb.core.components.bufferManager.bufferPool.replacementStrategies.PageReplacementStrategy;
 import ubadb.core.components.bufferManager.bufferPool.replacementStrategies.fifo.FIFOReplacementStrategy;
 import ubadb.core.components.catalogManager.CatalogManager;
-import ubadb.core.components.diskManager.DiskManager;
+import ubadb.core.components.catalogManager.CatalogManagerException;
+import ubadb.core.components.catalogManager.CatalogManagerImpl;
 import ubadb.external.bufferManagement.etc.BufferManagementMetrics;
 import ubadb.external.bufferManagement.etc.FaultCounterDiskManagerSpy;
 import ubadb.external.bufferManagement.etc.PageReference;
@@ -18,30 +23,82 @@ import ubadb.external.bufferManagement.etc.PageReferenceTraceSerializer;
 public class MainEvaluator
 {
 	private static final int PAUSE_BETWEEN_REFERENCES	= 0;
+	private static final String[] BUFFERPOOL_NAMES = {"DEFAULT", "KEEP", "RECYCLE"};
+	private static final int[] BUFFERPOOL_SIZES = {80, 30, 10};
+	private static final String CATALOGFILEPATH = "generated/catalog.db";
+	private static final String FILEPATHPREFIX = "";
 	
 	public static void main(String[] args)
 	{
-		try
-		{
-			PageReplacementStrategy pageReplacementStrategy = new FIFOReplacementStrategy();
-			String traceFileName = "generated/fileScan-Company.trace";
-			int bufferPoolSize = 100;
+		
+		PageReplacementStrategy pageReplacementStrategy = new FIFOReplacementStrategy();
+		PageReplacementStrategy pageReplacementStrategySingle = new FIFOReplacementStrategy();
+		//String traceFileName = "generated/prueba.trace";
+		String traceFileName = "generated_seq/prueba.trace";		
+
+		
+		Map<String, PageReplacementStrategy> pageReplacementStrategies = new HashMap<String, PageReplacementStrategy>();
+		for(int i = 0; i < BUFFERPOOL_NAMES.length; i++)
+			pageReplacementStrategies.put(BUFFERPOOL_NAMES[i], pageReplacementStrategy);
 			
-			evaluate(pageReplacementStrategy, traceFileName, bufferPoolSize);
+		Map<String, Integer> maxBufferPoolSizes = new HashMap<String, Integer>();
+		
+		int totalSize=0;
+		for(int i = 0; i < BUFFERPOOL_NAMES.length; i++)
+		{	
+			maxBufferPoolSizes.put(BUFFERPOOL_NAMES[i], BUFFERPOOL_SIZES[i]);
+			totalSize+=BUFFERPOOL_SIZES[i];
 		}
-		catch(Exception e)
-		{
-			System.out.println("FATAL ERROR (" + e.getMessage() + ")");
+		
+		CatalogManager catalogManager = new CatalogManagerImpl(CATALOGFILEPATH, FILEPATHPREFIX);
+		
+		try {
+			catalogManager.loadCatalog();
+		} catch (CatalogManagerException e1) {
+			// 
+			e1.printStackTrace();
+		}
+		
+		// Buffer Manager con múltiples buffer pools		
+		BufferPool multipleBufferPool = new MultipleBufferPool(maxBufferPoolSizes, pageReplacementStrategies, catalogManager);
+		FaultCounterDiskManagerSpy faultCounterDiskManagerSpy = new FaultCounterDiskManagerSpy();
+		BufferManager bufferManager = new BufferManagerImpl(faultCounterDiskManagerSpy, catalogManager, multipleBufferPool);
+
+		// Buffer Manager con un sólo buffer pool	
+		BufferPool singleBufferPool = new SingleBufferPool(totalSize,pageReplacementStrategySingle);
+		FaultCounterDiskManagerSpy faultCounterDiskManagerSpySingle = new FaultCounterDiskManagerSpy();
+		BufferManager bufferManagerSingle = new BufferManagerImpl(faultCounterDiskManagerSpySingle, catalogManager, singleBufferPool);
+		
+		PageReferenceTrace trace = null;
+		try {
+			trace = getTrace(traceFileName);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			evaluate(traceFileName, bufferManager);
+			evaluate(traceFileName, bufferManagerSingle);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		System.out.println("Multiple:");
+		System.out.println("---------");
+		BufferManagementMetrics metrics = new BufferManagementMetrics(trace, faultCounterDiskManagerSpy.getFaultsCount());
+		metrics.showSummary();
+
+		System.out.println("");
+		System.out.println("Single:");
+		System.out.println("-------");
+		metrics = new BufferManagementMetrics(trace, faultCounterDiskManagerSpySingle.getFaultsCount());
+		metrics.showSummary();
+		
 	}
 
-	private static void evaluate(PageReplacementStrategy pageReplacementStrategy, String traceFileName, int bufferPoolSize) throws Exception, InterruptedException, BufferManagerException
+	private static void evaluate(String traceFileName, BufferManager bufferManager) throws Exception
 	{
-		FaultCounterDiskManagerSpy faultCounterDiskManagerSpy = new FaultCounterDiskManagerSpy();
-//		CatalogManager catalogManager = new CatalogManagerImpl();
-		CatalogManager catalogManager = null;
-		BufferManager bufferManager = createBufferManager(faultCounterDiskManagerSpy, catalogManager, pageReplacementStrategy, bufferPoolSize);
+		
 		PageReferenceTrace trace = getTrace(traceFileName);
 		
 		for(PageReference pageReference : trace.getPageReferences())
@@ -72,21 +129,11 @@ public class MainEvaluator
 			}
 		}
 		
-		BufferManagementMetrics metrics = new BufferManagementMetrics(trace, faultCounterDiskManagerSpy.getFaultsCount());
-		metrics.showSummary();
 	}
 
 	private static PageReferenceTrace getTrace(String traceFileName) throws Exception
 	{
 		PageReferenceTraceSerializer serializer = new PageReferenceTraceSerializer();
 		return serializer.read(traceFileName);
-	}
-
-	private static BufferManager createBufferManager(DiskManager diskManager, CatalogManager catalogManager, PageReplacementStrategy pageReplacementStrategy, int bufferPoolSize)
-	{
-		BufferPool singleBufferPool = new SingleBufferPool(bufferPoolSize, pageReplacementStrategy);
-		BufferManager bufferManager = new BufferManagerImpl(diskManager, catalogManager, singleBufferPool);
-		
-		return bufferManager;
 	}
 }
--- TP2/UBADB/src/main/java/ubadb/external/bufferManagement/SeqMainTraceGenerator.java
+++ TP2/UBADB/src/main/java/ubadb/external/bufferManagement/SeqMainTraceGenerator.java
@@ -2,6 +2,7 @@ package ubadb.external.bufferManagement;
 
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -14,7 +15,7 @@ import ubadb.external.bufferManagement.traceGenerators.IndexScanTraceGenerator;
 import ubadb.external.bufferManagement.traceGenerators.MixedTraceGenerator;
 
 @SuppressWarnings("unused")
-public class MainTraceGenerator
+public class SeqMainTraceGenerator
 {
 	public static void main(String[] args) throws Exception
 	{
@@ -27,29 +28,30 @@ public class MainTraceGenerator
 		PageReferenceTraceSerializer serializer = new PageReferenceTraceSerializer();
 		
 		// Lectura aleatoria de tabla Materias
-		String fileNameA2 = "generated/trazas/01-indexScanUnclustered-Materias.trace";
+		String fileNameA2 = "generated_seq/trazas/01-indexScanUnclustered-Materias.trace";
 		PageReferenceTrace traceA2 = new IndexScanTraceGenerator().generateIndexScanUnclusteredForASingleLeaf(1,"Materias", 3, 800, 40);
 		serialize(fileNameA2, traceA2, serializer);
-			
+		
 		// Filescan de una tabla grande
-		String fileNameA1 = "generated/trazas/02-fileScan-Cursadas.trace";
+		String fileNameA1 = "generated_seq/trazas/02-fileScan-Cursadas.trace";
 		PageReferenceTrace traceA1 = new FileScanTraceGenerator().generateFileScan(1, "Cursadas", 250);
 		serialize(fileNameA1, traceA1, serializer);
 		
 		// Lectura aleatoria de tabla Estudiantes
-		String fileNameA3 = "generated/trazas/03-indexScanUnclustered-Estudiantes.trace";
+		String fileNameA3 = "generated_seq/trazas/03-indexScanUnclustered-Estudiantes.trace";
 		PageReferenceTrace traceA3 = new IndexScanTraceGenerator().generateIndexScanUnclusteredForASingleLeaf(1,"Estudiantes", 3, 300, 100);
 		serialize(fileNameA3, traceA3, serializer);
 		
 		// Lectura aleatoria de tabla Materias
-		String fileNameA4 = "generated/trazas/04-indexScanUnclustered-Materias.trace";
+		String fileNameA4 = "generated_seq/trazas/04-indexScanUnclustered-Materias.trace";
 		PageReferenceTrace traceA4 = new IndexScanTraceGenerator().generateIndexScanUnclusteredForASingleLeaf(1,"Materias", 3, 800, 40);
 		serialize(fileNameA4, traceA4, serializer);
 		
 		// Lectura aleatoria de tabla Estudiantes
-		String fileNameA5 = "generated/trazas/05-indexScanUnclustered-Estudiantes.trace";
+		String fileNameA5 = "generated_seq/trazas/05-indexScanUnclustered-Estudiantes.trace";
 		PageReferenceTrace traceA5 = new IndexScanTraceGenerator().generateIndexScanUnclusteredForASingleLeaf(1,"Estudiantes", 3, 300, 100);
 		serialize(fileNameA5, traceA5, serializer);
+		
 	}
 	
 	private static void serialize(String fileName, PageReferenceTrace trace, PageReferenceTraceSerializer serializer) throws Exception
@@ -61,33 +63,56 @@ public class MainTraceGenerator
 	private static void complexDataSet() throws Exception
 	{
 		PageReferenceTraceSerializer serializer = new PageReferenceTraceSerializer();
-		
-		String folderA = "generated/trazas";
-		String fileA = "generated/prueba.trace";
-		mixTraces(fileA,folderA,15,5,serializer);
+		String folderA = "generated_seq/trazas";
+		String fileA = "generated_seq/prueba.trace";
+		mixTraces(fileA,folderA,serializer);
 	}
 
-	private static void mixTraces(String fileNameForNewTrace, String folderName, int totalTracesCount, int maxConcurrentTracesCount, PageReferenceTraceSerializer serializer) throws Exception
+	private static void mixTraces(String fileNameForNewTrace, String folderName, PageReferenceTraceSerializer serializer) throws Exception
 	{
-		List<PageReferenceTrace> tracesToMix = buildTracesToMix(folderName,totalTracesCount,serializer);
+		List<PageReferenceTrace> tracesToMix = buildTracesToMix(folderName,serializer);
+	
+		PageReferenceTrace result = new PageReferenceTrace();
 		
-		PageReferenceTrace mixedTrace = new MixedTraceGenerator().generateMixedTrace(tracesToMix, totalTracesCount, maxConcurrentTracesCount);
+		while(!tracesToMix.isEmpty())
+		{
+			//Pick any concurrent trace
+			PageReferenceTrace trace = tracesToMix.get(0);
+			
+			while (!trace.getPageReferences().isEmpty()) {
+				result.addPageReference(trace.getPageReferences().get(0));
+				trace.getPageReferences().remove(0);
+			}
+			
+			tracesToMix.remove(0);
+			
+//			if(totalTracesQueue.peek() != null)
+//				concurrentTraces.add(totalTracesQueue.poll());
+//			}
+		}
 		
-		serializer.write(mixedTrace, fileNameForNewTrace);
+		
+		
+		
+//		PageReferenceTrace mixedTrace = new MixedTraceGenerator().generateMixedTrace(tracesToMix, totalTracesCount, maxConcurrentTracesCount);
+		
+		serializer.write(result, fileNameForNewTrace);
 		System.out.println("File " + fileNameForNewTrace + " generated");
 	}
 
-	private static List<PageReferenceTrace> buildTracesToMix(String folderName, int totalTracesCount, PageReferenceTraceSerializer serializer) throws Exception
+	private static List<PageReferenceTrace> buildTracesToMix(String folderName, PageReferenceTraceSerializer serializer) throws Exception
 	{
 		List<PageReferenceTrace> tracesToMix = new ArrayList<>();
-		Random random = new Random(System.currentTimeMillis());
+		List<String> traceFiles = new ArrayList<String>();
 		
-		String[] traceFiles = Paths.get(folderName).toFile().list();
+		for (String file : Paths.get(folderName).toFile().list()) {
+			traceFiles.add(file);
+		}
+		Collections.sort(traceFiles);
 		
-		for(int i=0; i < totalTracesCount; i++)
-		{
-			int anyTraceIndex = random.nextInt(traceFiles.length); 
-			PageReferenceTrace anyTrace = serializer.read(folderName + "/" + traceFiles[anyTraceIndex]);
+		for(int i=0; i < traceFiles.size(); i++)
+		{ 
+			PageReferenceTrace anyTrace = serializer.read(folderName + "/" + traceFiles.get(i));
 			anyTrace.changeTransactionId(new TransactionId(i));
 			
 			tracesToMix.add(anyTrace);
